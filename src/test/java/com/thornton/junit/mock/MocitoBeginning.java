08d745f0fd709b6f4533adc9d426ca567b92629c
Nagaraju Ankamreddi <Sanju@Nagarajus-MacBook-Pro.local>
2020-02-04 21:50:10 -0700

    first commit

---
+++ src/test/java/com/thornton/junit/mock/MocitoBeginning.java
@@ -0,0 +1,13 @@
+package com.thornton.junit.mock;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MocitoBeginning
+{
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
